3d51ca76198af0e881874775d8fc3c88cf27e3e4
leejungdo <ljd100000@gmail.com>
2019-08-08 17:08:41 +0900

    2

---
+++ src/main/java/mybatis/mybatis_test1/TestClass.java
@@ -0,0 +1,19 @@
+package mybatis.mybatis_test1;
+
+public class TestClass {
+	int i;
+	int j;
+	public int getI() {
+		return i;
+	}
+	public void setI(int i) {
+		this.i = i;
+	}
+	public int getJ() {
+		return j;
+	}
+	public void setJ(int j) {
+		this.j = j;
+	}
+	
+}
